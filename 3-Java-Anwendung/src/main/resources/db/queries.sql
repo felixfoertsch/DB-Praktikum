e1396e4522d0936eabac5aaaca7ad5bfce963e35
Felix Förtsch <felixfoertsch@gmail.com>
2019-05-30 10:16:17 +0200

    add queries 1-7

--- 3-Java-Anwendung/src/main/resources/db/queries.sql
+++ 3-Java-Anwendung/src/main/resources/db/queries.sql
@@ -46,12 +46,59 @@ WHERE jahr = 2017
   AND erschienen = TRUE
 
 -- 4. Welche Mitarbeiter der Abteilung haben noch nie eine Abschlussklausur beaufsichtigt?
+-- Finde alle Mitarbeiter, die AKs beaufsichtigt haben. Gib restliche Mitarbeiter zurück.
+SELECT vorname, nachname
+FROM mitarbeiter
+WHERE id NOT IN (SELECT mitarbeiterid
+             FROM klausur
+                      JOIN abschlussklausur a on klausur.id = a.klausurid
+                      JOIN aufsicht a2 on klausur.id = a2.klausurid);
 
 -- 5. Welche Veranstaltungsreihen wurden immer zur selben Zeit abgehalten?
+-- Annahmen: Wochentag egal, Veranstaltungsreihe = gleicher Name
+-- Also alle, die min. 1 mal vorkommen und zur selben Zeit stattfinden
+-- abhaltung join veranstaltung, group by name & zeit, alle raus bei denen Name öfter als 1 Mal vorkommt, weil die zu unterschiedlichen Zeiten abgehalten wurden.
+SELECT name
+FROM (SELECT name,
+             zeit
+      FROM veranstaltung
+               JOIN abhaltung on veranstaltung.id = abhaltung.veranstaltungid
+      GROUP BY name, zeit) AS groupedVeranstaltungen
+GROUP BY name
+HAVING COUNT(name) < 2;
 
 -- 6. Welche Studenten haben nur Grundvorlesungen besucht?
+-- D.h. GV wurde besucht, aber SV wurde nicht besucht.
+-- Annahme: Besucht heißt, Klausur wurde in dem Modul geschrieben.
+-- Alle Studenten, die GV besucht haben minus die, die SV besucht haben
+SELECT DISTINCT s2.id, s2.vorname, s2.nachname
+FROM klausur
+         JOIN studentteilnahmeklausur s on klausur.id = s.klausurid
+         JOIN student s2 on s.studentid = s2.id
+WHERE grundvorlesungid IS NOT NULL
+    EXCEPT
+SELECT DISTINCT s2.id, s2.vorname, s2.nachname
+FROM klausur
+         JOIN studentteilnahmeklausur s on klausur.id = s.klausurid
+         JOIN student s2 on s.studentid = s2.id
+WHERE spezialvorlesungid IS NOT NULL;
 
 -- 7. Erstellen Sie eine Liste aller Studierenden geordnet nach der Anzahl der erfolgreich teilgenommenen Klausuren sowie Prüfungsleistungen.
+-- erfolgreich = Note zwischen 1.0 und 4.0 (nicht erschienen hat Note 0.0)
+SELECT id, vorname, nachname, teilnahmen
+FROM (SELECT studentid, COUNT(studentid) as teilnahmen
+      FROM (SELECT studentid
+            FROM studentteilnahmeklausur
+            WHERE note >= 1.0
+              AND note <= 4.0
+            UNION ALL
+            SELECT studentid
+            FROM studentteilnahmeveranstaltung
+            WHERE note >= 1.0
+              AND note <= 4.0) AS veranstaltungen
+      GROUP BY studentid) AS gesamt
+         JOIN student ON gesamt.studentid = student.id
+ORDER BY teilnahmen DESC;
 
 -- 8. Welche Studenten haben im Jahr 2016 und 2017 jeweils mindestens zwei Veranstaltungen zusammen besucht.
 
