c573ef5651bffcf666329322b3e05a2951dc8197
Felix Förtsch <felixfoertsch@gmail.com>
2019-06-26 13:08:06 +0200

    Link FXML GridPane to controller

--- 3-Java-Anwendung/src/main/java/controller/KlausurStatistikController.java
+++ 3-Java-Anwendung/src/main/java/controller/KlausurStatistikController.java
@@ -1,11 +1,17 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.GridPane;
 import model.klausur.Klausur;
 import org.hibernate.SessionFactory;
+
 
 public class KlausurStatistikController {
     private SessionFactory sessionFactory;
     private Klausur klausur;
+    @FXML
+    private GridPane klausurStatistikGridPane;
+
 
     void injectSessionFactory(SessionFactory sessionFactory) {
         this.sessionFactory = sessionFactory;
